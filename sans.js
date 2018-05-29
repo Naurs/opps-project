e9353d773b5c0025d91ddda018f3ed30ab7b13ef
Naurs <35874356+Naurs@users.noreply.github.com>
2018-05-29 13:00:46 +0200

    Sprawdzanie, czy bot działa na serwerze discord

--- sans.js
+++ sans.js
@@ -1,1 +1,12 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
+bot.on('message', (message) =>{
+	
+	if(message.content == 'ping'){
+		messege.channel.sendMessage('pong');
+	}
+	
+});
+
+bot.login('token do bota');
