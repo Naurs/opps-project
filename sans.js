756fe0d4575d7e3aae69cea5527b8761e067a269
Naurs <35874356+Naurs@users.noreply.github.com>
2018-06-04 17:23:54 +0200

    Dodanie pliku konfiguracyjnego bota z tokenem i z prefixem, komenda ping i avatar

--- sans.js
+++ sans.js
@@ -1,16 +1,25 @@
 const Discord = require('discord.js');
+const botconfig = require('./botconfig.json');
 const client = new Discord.Client();
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Undertale Literature Club!", 'https://www.twitch.tv/naurs07');
+  client.user.setStatus("dnd");
+});
+      
+client.on('message', msg => {
+  let prefix = botconfig.prefix;
+  let timems = Date.now() - msg.createdTimestamp;
+
+  if (msg.content == `${prefix}ping`)  {
+    msg.channel.send('Pong!🏓 '+ `${timems}` + ' ms');
+  }
+  if (msg.content == `${prefix}avatar`) {
+    msg.channel.send(msg.author.avatarURL);
+  }
 });
 
-client.on('message', msg => {
-  if (msg.content == '-ping')  {
-    msg.reply('Pong!');
-  }
-  if (msg.content == '-avatar') {
-    msg.reply(msg.author.avatarURL);
-  }
-});
-client.login('token');
+
+client.login(botconfig.token);
