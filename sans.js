d4bf0e0c4ee15faa2d939c1e5c48b69a81a22124
Naurs <35874356+Naurs@users.noreply.github.com>
2018-06-05 20:33:15 +0200

    dodanie podstawowych informacji o bocie i serwerze

--- sans.js
+++ sans.js
@@ -8,16 +8,41 @@ client.on('ready', () => {
   client.user.setActivity("Undertale Literature Club!", 'https://www.twitch.tv/naurs07');
   client.user.setStatus("dnd");
 });
-      
+
 client.on('message', msg => {
   let prefix = botconfig.prefix;
-  let timems = Date.now() - msg.createdTimestamp;
 
   if (msg.content == `${prefix}ping`)  {
-    msg.channel.send('Pong!🏓 '+ `${timems}` + ' ms');
+    msg.channel.send('Pong!🏓 '+ `${Date.now() - msg.createdTimestamp}` + ' ms');
   }
   if (msg.content == `${prefix}avatar`) {
     msg.channel.send(msg.author.avatarURL);
+  }
+  if (msg.content == `${prefix}botinfo`){
+    const embed = new Discord.RichEmbed()
+    .setDescription("BOT INFO")
+    .setColor(0x2839FF)
+    .setThumbnail(client.user.displayAvatarURL)
+    .addField("Bot name:", client.user.username)
+    .addField("Bot created:", client.user.createdAt  )
+    .setFooter('Made by Naurs')
+    .setTimestamp();
+
+    msg.channel.send({embed});
+  }
+  if (msg.content == `${prefix}serverinfo`){
+    const embed = new Discord.RichEmbed()
+    .setDescription("BOT INFO")
+    .setColor(0x2839FF)
+    .setThumbnail(client.user.displayAvatarURL)
+    .addField("Sever name:", msg.guild.name)
+    .addField("Created On :", msg.guild.createdAt)
+    .addField("You joined:", msg.member.joinedAt)
+    .addField("All users in server :", msg.guild.memberCount)
+    .setFooter('Made by Naurs')
+    .setTimestamp();
+
+    msg.channel.send({embed});
   }
 });
 
