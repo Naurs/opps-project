86e6e3c3e6a20344f369fea52af0260c725b52d0
Naurs <35874356+Naurs@users.noreply.github.com>
2018-06-18 11:23:56 +0200

    poprawienie kick, dodanie opcji ban
    i mała modyfikacja kodu

--- sans.js
+++ sans.js
@@ -9,17 +9,19 @@ client.on('ready', () => {
   client.user.setStatus("dnd");
 });
 
-client.on('message', msg => {
+client.on('message', message => {
   let prefix = botconfig.prefix;
-  let cmd = msg.content;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
-  if (cmd == `${prefix}ping`)  {
-   msg.channel.send('Pong!🏓 '+ `${msg.client.ping}` + ' ms');
+  if (cmd === `${prefix}ping`)  {
+   message.channel.send('Pong!🏓 '+ `${message.client.ping}` + ' ms');
   }
-  if (cmd == `${prefix}avatar`) {
-    msg.channel.send(msg.author.avatarURL);
+  if (cmd === `${prefix}avatar`) {
+    message.channel.send(message.author.avatarURL);
   }
-  if (cmd == `${prefix}botinfo`){
+  if (cmd === `${prefix}botinfo`){
     const embed = new Discord.RichEmbed()
     .setDescription("BOT INFO")
     .setColor(0x2839FF)
@@ -29,35 +31,54 @@ client.on('message', msg => {
     .setFooter('Made by Naurs')
     .setTimestamp();
 
-    msg.channel.send({embed});
+    message.channel.send({embed});
   }
-  if (cmd == `${prefix}serverinfo`){
+  if (cmd === `${prefix}serverinfo`){
     const embed = new Discord.RichEmbed()
     .setDescription("BOT INFO")
     .setColor(0x2839FF)
     .setThumbnail(client.user.displayAvatarURL)
-    .addField("Sever name:", msg.guild.name)
-    .addField("Created On :", msg.guild.createdAt)
-    .addField("You joined:", msg.member.joinedAt)
-    .addField("All users in server :", msg.guild.memberCount)
+    .addField("Sever name:", message.guild.name)
+    .addField("Created On :", message.guild.createdAt)
+    .addField("You joined:", message.member.joinedAt)
+    .addField("All users in server :", message.guild.memberCount)
     .setFooter('Made by Naurs')
     .setTimestamp();
 
-    msg.channel.send({embed});
+    message.channel.send({embed});
   }
-  if(cmd == `${prefix}kick`){
-    let kUser = msg.guild.member(msg.mentions.user);
-    if(!kUser){
-      msg.channel.send("Can't find user!!!");
+  if(cmd === `${prefix}kick`){
+    let kUser = message.guild.member(message.mentions.users.first());
+    if(message.member.hasPermission("MANAGE_MESSAGES")){
+      let bReason = args.join(" ").slice(22);
+      if(!kUser){
+        return message.channel.send("Can't find user!!!");
+      } else if(kUser.hasPermission("MANAGE_MESSAGES")){
+        return message.channel.send("This man is good, also i can't kick");
+      } else{
+        kUser.ban(kReason); 
+        message.channel.send("(Not)Pal has kicked");
+      }
+    } else{
+      message.channel.send("You have no power here!");
     }
-    if(!msg.member.hasPermission("MANAGE_MESSAGES")){
-      msg.channel.send("You have no power here");
+  }
+
+  if(cmd === `${prefix}ban`){
+    let bUser = message.guild.member(message.mentions.users.first());
+    if(message.member.hasPermission("MANAGE_MESSAGES")){
+      let bReason = args.join(" ").slice(22);
+      if(!bUser){
+        return message.channel.send("Can't find user!!!");
+      } else if(bUser.hasPermission("MANAGE_MESSAGES")){
+        return message.channel.send("This man is good, also i can't ban");
+      } else{
+        bUser.ban(bReason); 
+        message.channel.send("(Not)Pal has banned🔨");
+      }
+    } else{
+      message.channel.send("You have no power here!");
     }
-    if(kUser.hasPermission){
-      msg.channel.send("This man is good, also i can't kick");
-    }
-    msg.guild.member(kUser).kick("Bo tak"); 
-    msg.channel.send("Pal has kicked");
   }
 });
 
